db1f1e53645d58ea4ae794e0f9c42d6388758279
KishmatSapkota <kishmatsapkota47@gmail.com>
2025-07-14 13:10:58 +0545

    connecting to github

---
+++ Java Script/script.js
@@ -0,0 +1,19 @@
+// function hi(){
+//     alert("HELLO WORLD");
+// }
+
+// function hi(){
+//     alert("HELLO USERS");
+// }
+
+// function hi(){
+//     let a = 10;
+//     let b = 20;
+//     let c = a + b;
+//     alert(c);
+// }
+
+function change(){
+    document.getElementById('home').innerHTML="visit now";
+
+}
